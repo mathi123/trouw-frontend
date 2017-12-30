e795bffa9b67ae711bd9c6e6b6993ab503e1cd7f
Mathias Colpaert <colpaert.mathias@gmail.com>
2017-12-30 13:12:34 +0100

    update port

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  api: 'http://telefon-api.mitasco.be/api',
-  public: 'http://telefon-api.mitasco.be/public/files',
+  api: 'http://telefon-api.mitasco.be:3000/api',
+  public: 'http://telefon-api.mitasco.be:3000/public/files',
   frontend: 'http://telefon.mitasco.be',
-  samples: 'http://telefon-api.mitasco.be/public/samples',
+  samples: 'http://telefon-api.mitasco.be:3000/public/samples',
 };
