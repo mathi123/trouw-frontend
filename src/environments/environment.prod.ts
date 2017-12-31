0fe4c116beb08dcc2677a9fc59595f0932863bf0
Mathias Colpaert <colpaert.mathias@gmail.com>
2017-12-31 09:58:05 +0100

    add https to frontend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   api: 'http://telefon-api.mitasco.be:3000/api',
   public: 'http://telefon-api.mitasco.be:3000/public/files',
-  frontend: 'http://telefon.mitasco.be',
+  frontend: 'https://telefon.mitasco.be',
   samples: 'http://telefon-api.mitasco.be:3000/public/samples',
 };
