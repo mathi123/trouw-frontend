616ea99c6cd108817e2c990e51f27b9b66c4278e
Mathias Colpaert <colpaert.mathias@gmail.com>
2018-01-24 07:21:27 +0100

    yolo

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Location } from '@angular/common';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-login',
@@ -8,10 +9,13 @@ import { Location } from '@angular/common';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  public showFeest = false;
+
   constructor(private location: Location) {
 
    }
   ngOnInit() {
+    this.showFeest = environment.feest;
   }
 
   public back() {
